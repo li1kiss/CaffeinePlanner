973f4668545b4a43136f044b3114cddcb3eea4aa
Mykhailo Kravchuk <mishakr232@gmail.com>
2025-01-23 15:46:42 +0100

    Initial Commit

---
+++ CaffeinePlanner/CaffeinePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinePlannerApp.swift
+//  CaffeinePlanner
+//
+//  Created by Mykhailo Kravchuk on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaffeinePlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaffeinePlanner
+//
+//  Created by Mykhailo Kravchuk on 23/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
